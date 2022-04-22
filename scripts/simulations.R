b9f52575e9c0fdb18c6b2fe89193995ee3bd8530
Daniel Fridljand <fridljan@seneca.embl.de>
2022-04-22 09:37:21 +0200

    about to upload smaller simulation

---
+++ scripts/simulations.R
@@ -0,0 +1,45 @@
+#parent
+library(magrittr)
+library(dplyr)
+
+#library(doParallel)
+#n.cores <- parallel::detectCores()
+#doParallel::registerDoParallel(cores = min(5, n.cores - 1))
+#library(doRNG)
+#set.seed(123)
+
+#library(IHWStatsPaper)
+# TODO
+# devtools::load_all("../IHW")
+# library("IHW")
+devtools::load_all("IHWForestPaper")
+
+##---parameters----
+m <- 5e4 #TODO more
+r <- 200
+r <- 1 # number of monte carlo replicates, increases run time immensely!
+alpha <- .1
+
+#folds_fdp_eval <- sample(1:3, m, replace = TRUE)
+
+forest_par <- list(
+  ntrees = 3,
+  ntaus = 10,
+  nsplit = 4,
+  maxdepth = 4,
+  nodesize = "auto"
+)
+## -----small region sim------
+lengths <- seq(from = 1, to = 1001, by = 250)
+#lengths <- 2
+
+eval_small_region_sim <- IHWForestPaper::eval_small_region_sim(m, r, lengths, forest_par)
+saveRDS(eval_small_region_sim, file = "precomputed_results/small_region_sim.Rds")
+
+## -----noise sim------
+dimensions <- seq(from = 1, to = 6, by = 1)
+#dimensions <- 2
+
+#eval_noise_sim <- IHWForestPaper::eval_noise_sim(m, r, dimensions, forest_par)
+#saveRDS(eval_noise_sim, file = "precomputed_results/noise_sim.Rds")
+
