de1c0f65df11bea33bd8540b54af3cc6461d9294
Daniel Fridljand <fridljan@seneca.embl.de>
2022-05-16 15:21:23 +0200

    progress

--- scripts/download_maf.R
+++ scripts/download_maf.R
@@ -1,15 +1,13 @@
 # download MAF data using Biomart
-
-library(biomaRt)
 library(magrittr)
 library(dplyr)
 library(tidyr)
 
-
+#load snps
 snp_chr1 <- readRDS("data/hqtl_chrom1_chrom2/snppos_chr1.Rds") 
 snp_chr2 <- readRDS("data/hqtl_chrom1_chrom2/snppos_chr2.Rds") 
 
-ensembl <- useMart("ENSEMBL_MART_SNP", dataset = "hsapiens_snp")
+ensembl <- biomaRt::useMart("ENSEMBL_MART_SNP", dataset = "hsapiens_snp")
 
 chr1_maf <- biomaRt::getBM(
   attributes = c("refsnp_id", "minor_allele_freq"),
