829cd4ae6489944987e93d0f7d3852fc1eea5dd7
Daniel Fridljand <daniel.fridljand@gmail.com>
2023-07-14 12:07:11 -0700

    add in bag filtering

--- IHWForestPaper/R/helper.R
+++ IHWForestPaper/R/helper.R
@@ -35,12 +35,14 @@ fdp_eval <- function(Hs, rjs){
 run_sim <- function(Ps, Xs, Hs, seed, alpha=0.1, m=10000, lfdr_only=FALSE, forest_par, null_proportion = T){
   ihw_quantile_res <-  error_fdp_table(try(fdp_eval(Hs, ihw_quantile_wrapper(Ps, Xs, alpha, null_proportion = null_proportion))))
   
-  ihw_forest_res <-  error_fdp_table(try(fdp_eval(Hs, ihw_forest_wrapper(Ps, Xs, alpha, forest_par, null_proportion = null_proportion)))) 
+  ihw_forest_res <-  error_fdp_table(try(fdp_eval(Hs, ihw_forest_wrapper(Ps, Xs, alpha, forest_par, null_proportion = null_proportion, drop_inbag = FALSE)))) 
+  ihw_forest_drop_inbag_res <-  error_fdp_table(try(fdp_eval(Hs, ihw_forest_wrapper(Ps, Xs, alpha, forest_par, null_proportion = null_proportion, drop_inbag = TRUE)))) 
   
   bh_res <- fdp_eval(Hs,  p.adjust(Ps, method="BH") <= alpha)
   
   sim_res <-  bind_rows(mutate(ihw_quantile_res, method="IHW-quantile"),
                         mutate(ihw_forest_res, method="IHW-forest"),
+                        mutate(ihw_forest_drop_inbag_res, method="IHW-forest-drop-inbag"),
                         mutate(bh_res, method="BH"))
   
   if (!lfdr_only){
--- IHWForestPaper/R/ihw_wrapper.R
+++ IHWForestPaper/R/ihw_wrapper.R
@@ -36,16 +36,19 @@ ihw_quantile_wrapper <- function(Ps, Xs, alpha, per_covariate_bins = 5, null_pro
 #' @param alpha    Significance level at which to apply method
 #' @param Storey   Bool (default: FALSE): is the procedure pi0 adaptive or not?
 #' @param forest_par  TODO
-#'
+#' @param drop_inbag do not use hypotheses, which have already been used for the 
+#'        construction of the partition for the construction of the weights as well
 #' @return         Binary vector of rejected/non-rejected hypotheses.
 #'
 #' @export
-ihw_forest_wrapper <- function(Ps, Xs, alpha, forest_par, null_proportion = T, per_covariate_bins = 5) {
+ihw_forest_wrapper <- function(Ps, Xs, alpha, forest_par, null_proportion = T, per_covariate_bins = 5, drop_inbag = FALSE) {
   #number_covariates <- ncol(Xs)
   
   #nodedepth <- number_covariates * log2(per_covariate_bins)
   #nbins_quantile <- per_covariate_bins^number_covariates
   #nodesize <- ceiling(length(Ps)/nbins_quantile)
+  ntrees <-forest_par$ntrees
+  if(drop_inbag) ntrees <- 10*ntrees
 
   ihw_forest <- IHW::ihw(Ps, Xs, alpha,
     stratification_method = "forest", null_proportion = null_proportion,
@@ -55,7 +58,8 @@ ihw_forest_wrapper <- function(Ps, Xs, alpha, forest_par, null_proportion = T, p
     nodesize = forest_par$nodesize, 
     #nodedepth = nodedepth,
     #nodesize = nodesize,
-    lambdas = Inf
+    lambdas = Inf,
+    drop_inbag = drop_inbag
   )
   
   IHW::rejected_hypotheses(ihw_forest)
