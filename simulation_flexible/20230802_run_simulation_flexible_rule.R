946b878b60e8ffc0ff65518d6a62ed632b25cf12
Daniel Fridljand <daniel.fridljand@gmail.com>
2023-08-02 18:51:58 -0700

    about to introduce split index

--- simulation_flexible/20230802_run_simulation_flexible_rule.R
+++ simulation_flexible/20230802_run_simulation_flexible_rule.R
@@ -34,37 +34,49 @@ if (dry_run) {
   dimensions <- seq(from = 2, to = 2, by = 1)
   m <- 1000
   r <- 1
-  additional_arguments_prop_alt_function_creator <- data.frame(ndim = 1, 
-                                                               signal_strength = 0.9, 
-                                                               lp_norm = 1, 
-                                                               target_average_alt_prob = 0.1)
+  ndim = 1
+  signal_strength = 0.9
+  lp_norm = 1
+  target_average_alt_prob = 0.1
+  kappa = 0
 } else {
   dimensions <- seq(from = 2, to = 6, by = 1)
   m <- 10000
   r <- 5
-  additional_arguments_prop_alt_function_creator <- expand.grid(
-    ndim = c(2,3), #dimensions
-    signal_strength = seq(0.1, 0.8, length.out = 2),
-    lp_norm = c(1, 2),#c(0.5, 1, 2, 3, 4),
-    target_average_alt_prob = seq(0.01, 0.1, length.out = 2) #5
-  )
+  ndim = c(2,3)
+  signal_strength = seq(0.1, 0.8, length.out = 2)
+  lp_norm = c(1, 2)
+  target_average_alt_prob = seq(0.01, 0.1, length.out = 2)
+  kappa = 0
+  #additional_arguments_prop_alt_function_creator <- expand.grid(
+  #  ndim = c(2,3), #dimensions
+  #  signal_strength = seq(0.1, 0.8, length.out = 2),
+  #  lp_norm = c(1, 2),#c(0.5, 1, 2, 3, 4),
+  #  target_average_alt_prob = seq(0.01, 0.1, length.out = 2) #5
+  #)
 }
 
-
-set.seed(seed)
-cat("seed", seed, "\n")
-cat("dimensions", dimensions, "\n")
+cat(timestamp(),"\n")
 
 print("\n")
 
+##---parameters ---
+sim_parameters <- expand.grid(
+  seed = seq_len(r),
+  dimensions = dimensions,
+  m = m, 
+  kappa = kappa,
+  ndim = ndim,
+  signal_strength = signal_strength,
+  lp_norm = lp_norm,
+  target_average_alt_prob = target_average_alt_prob
+)
+
 ##--create simulation---
 simulation_list <- flexible_prop_alt_sim(
-  m = m,
-  r = r,
-  dimensions = dimensions,
+  sim_parameters = sim_parameters,
   prop_alt_function_creator = prop_alt_function_creator,
-  prop_alt_function_name = prop_alt_function_name,
-  additional_arguments_prop_alt_function_creator = additional_arguments_prop_alt_function_creator
+  prop_alt_function_name = prop_alt_function_name
 )
 
 ##---evaluate mehtods on simulation---
@@ -72,6 +84,8 @@ evaluated_simulation <- eval_sim_parallel(simulation_list,
   alpha = 0.1,
   methods = c("IHW-quantile", "IHW-forest", "BH", "AdaPT", "Boca-Leek", "Clfdr-EM")
 )
+
+print("\n")
 
 ##---save result----
 saveRDS(evaluated_simulation, paste0(
