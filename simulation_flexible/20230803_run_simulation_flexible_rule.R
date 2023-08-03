dae137a1705361f15a8e67e94a186565c89560f3
Daniel Fridljand <daniel.fridljand@gmail.com>
2023-08-03 10:42:59 -0700

    not dry run

--- simulation_flexible/20230803_run_simulation_flexible_rule.R
+++ simulation_flexible/20230803_run_simulation_flexible_rule.R
@@ -11,7 +11,7 @@ if (length(commandArgs(trailingOnly = TRUE)) > 0) {
   split_index <- as.numeric(split_index)
   num_splits <- commandArgs(trailingOnly = TRUE)[2]
   num_splits <- as.numeric(num_splits)
-  dry_run <- TRUE
+  dry_run <- FALSE
 } else {
   num_splits <- 3
   split_index <- 2
